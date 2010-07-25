a0931445f62dc81cc04f01bed670f36ba5fcf284
Yuri Leikind <yuri.leikind@gmail.com>
2010-07-25 13:04:01 +0200

    two separate generators for each of the JS frameworks instead of one

---
+++ generators/assignment_lists_jquery/assignment_lists_jquery_generator.rb
@@ -0,0 +1,31 @@
+class AssignmentListsJqueryGenerator < Rails::Generator::Base
+  def active_js_framework
+    ':jquery'
+  end
+
+  def inactive_js_framework
+    ':prototype'
+  end
+  
+  def manifest
+    record do |m|
+      # js
+      m.file "javascripts/assignment_lists_jquery.js",  "public/javascripts/assignment_lists_jquery.js"
+      
+      # config
+      m.directory "config/initializers"
+      m.template "../../common_templates/initializers/ass_lists_config.rb",  "config/initializers/ass_lists_config.rb"
+
+      # css
+      m.file "../../common_templates/stylesheets/assignment_lists.css",  "public/stylesheets/assignment_lists.css"
+
+      # images
+      m.directory "public/images/icons/"
+      
+      %w(wal_edit.png wal_spinner.gif).each do |f|
+        m.file "../../common_templates/icons/#{f}",  "public/images/icons/#{f}"
+      end
+    end
+  end
+end
+
--- generators/common_templates/initializers/ass_lists_config.rb
+++ generators/common_templates/initializers/ass_lists_config.rb
@@ -1,7 +1,7 @@
 if defined?(AssignmentLists::Defaults)
 
-  # AssignmentLists::Defaults::JS_FRAMEWORK = :jquery
-  AssignmentLists::Defaults::JS_FRAMEWORK = :prototype
+  # AssignmentLists::Defaults::JS_FRAMEWORK = <%= inactive_js_framework %>
+  AssignmentLists::Defaults::JS_FRAMEWORK = <%= active_js_framework %>
 
   # The label on a button which moves items from the left list to the right list.
   AssignmentLists::Defaults::ADD_BUTTON_LABEL    = ' &gt;&gt; '
